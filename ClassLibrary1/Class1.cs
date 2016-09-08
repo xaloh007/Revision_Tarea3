dac35b7a69c11d9619a6b3d386a080e7bbcadf30
xaloh007 <xaloh.toloza@gmail.com>
2016-09-07 22:16:30 -0300

    Revision

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        var phrase = "lalalalalalalalalalalalalalalalalalalalalalalalalalalalalala";
+        var manyPhrases = new StringBuilder();
+        for (var i = 0; i< 10000; i++)
+        {
+            manyPhrases.Append(phrase);
+        }
+            //Console.WriteLine("tra" + manyPhrases);
+}
+}
